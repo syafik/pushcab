726664e6d45f23261515ed6f1e681941c5866e22
Syafik <syafiklijtk04@gmail.com>
2013-05-04 11:50:08 +0700

    setup mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,14 @@ Pushcab::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => 'pushcab.com',
+      :user_name            => 'pushjaw@gmail.com',
+      :password             => 'pushjaw123',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true  }
 end
