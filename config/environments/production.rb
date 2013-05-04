33ced6e8758be2c1f79bd2c6f2536edbd191e355
Syafik <syafiklijtk04@gmail.com>
2013-05-04 12:30:40 +0700

    setup mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,15 @@ Pushcab::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.default_url_options = { :host => 'pushcab.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => 'pushcab.com',
+      :user_name            => 'pushjaw@gmail.com',
+      :password             => 'pushjaw123',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true  }
 end
