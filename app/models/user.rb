1e4eab0f501e64598ca1625347cac74a1575ac02
Syafik <syafiklijtk04@gmail.com>
2013-05-04 10:26:47 +0700

    users sign in using their username or email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,22 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :lockable, :timeoutable, :token_authenticatable, :confirmable
+         :timeoutable, :token_authenticatable, :confirmable,
+         :authentication_keys => [:login]
 
+  attr_accessor :login
   attr_accessible :email, :password, :password_confirmation, :remember_me, :confirmed_at,
-    :phone_no, :username
+    :phone_no, :username, :login
   
-  validates :phone_no, presence: true, uniqueness: true
+  validates :phone_no, :username, presence: true, uniqueness: true
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
 end
