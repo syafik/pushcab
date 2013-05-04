7fc8296df7b8e3edd42c1092378b33c49dd5e0f4
Syafik <syafiklijtk04@gmail.com>
2013-05-04 09:43:56 +0700

    add data seeds for drivers and passengers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,21 @@
-driver = Driver.new(email: "user1@pushcab.com", phone_no: "081809159141", police_no: "D 1264 ER", username: "user1")
-driver.password = "123456"
-driver.password_confirmation = "123456"
-driver.save!
+[Driver, User].each(&:destroy_all)
+
+p "creating drivers"
+(1..5).each do |driver|
+  Driver.create!(email: "driver#{driver}@pushcab.com",
+                password: "password",
+                phone_no: "08180915914#{driver}",
+                police_no: "D126#{driver}ER",
+                username: "user#{driver}")
+end
+
+p "creating passenger"
+(1..5).each do |passenger|
+  User.create!(email: "passenger#{passenger}@pushcab.com",
+              password: "password",
+              phone_no: "12345#{passenger}",
+              username: "passenger#{passenger}")
+end
+
+#update all passenger confirmation account
+User.update_all(:confirmed_at => Time.now)
